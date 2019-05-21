9846a963587f1c1f66438058ab9a09c001e0b9a8
David Baker Effendi <20056354@sun.ac.za>
2019-05-21 18:59:27 +0200

    Removed JHipster generated client

--- providentia-client/src/main/webapp/app/admin/docs/docs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'prv-docs',
-  templateUrl: './docs.component.html'
-})
-export class PrvDocsComponent {
-  constructor() {}
-}
--- providentia-client/src/main/webapp/app/layouts/footer/footer.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'prv-footer',
-  templateUrl: './footer.component.html'
-})
-export class FooterComponent {}
