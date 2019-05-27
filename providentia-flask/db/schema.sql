f4c01695d9c47dde37cb43a49fc5f22ed797b993
David Baker Effendi <20056354@sun.ac.za>
2019-05-27 19:49:53 +0200

    Created entities and repositories to model the database better. These objects are serialized and deserialized accordingly.

--- providentia-flask/db/schema.sql
+++ providentia-flask/db/schema.sql
@@ -2,26 +2,45 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS public.graphs;
-DROP TABLE IF EXISTS public.results;
+DROP TABLE IF EXISTS graphs;
+DROP TABLE IF EXISTS results;
+DROP TABLE IF EXISTS datasets;
+DROP TABLE IF EXISTS databases;
 
-CREATE TABLE public.results (
+CREATE TABLE databases (
     id uuid DEFAULT uuid_generate_v4(),
-    database character varying(255) NOT NULL,
-    dataset character varying(255) NOT NULL,
-    date_executed timestamp without time zone NOT NULL,
-    title character varying(255) NOT NULL,
-    description character varying(255),
-    query_time integer NOT NULL,
-    analysis_time integer NOT NULL,
+    name character varying(255) NOT NULL,
+    description text NOT NULL,
+    icon text NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.graphs (
+CREATE TABLE datasets (
+    id uuid DEFAULT uuid_generate_v4(),
+    name character varying(255) NOT NULL,
+    description text NOT NULL,
+    icon text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE results (
+    id uuid DEFAULT uuid_generate_v4(),
+    database_id uuid NOT NULL,
+    dataset_id uuid NOT NULL,
+    date_executed timestamp without time zone NOT NULL,
+    title character varying(255) NOT NULL,
+    description text,
+    query_time integer NOT NULL,
+    analysis_time integer NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (database_id) REFERENCES databases (id),
+    FOREIGN KEY (dataset_id) REFERENCES datasets (id)
+);
+
+CREATE TABLE graphs (
     id uuid DEFAULT uuid_generate_v4(),
     result_id uuid NOT NULL,
-    graphSON text NOT NULL,
+    graphson text NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (result_id) REFERENCES results (id)
 );
-
