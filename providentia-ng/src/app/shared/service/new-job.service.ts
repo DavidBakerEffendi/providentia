71a5a28c0587b5faf40bc0957b2fb6f850e75bb2
David Baker Effendi <20056354@sun.ac.za>
2019-05-29 21:27:44 +0200

    Added new job service for API calls and updated IDE workspace configurations.

---
+++ providentia-ng/src/app/shared/service/new-job.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import APP_CONFIG from '../../app.config';
+import { IBenchmark } from '../model/benchmark.model';
+
+type EntityResponseType = HttpResponse<IBenchmark>;
+
+@Injectable({ providedIn: 'root' })
+export class NewJobService {
+    private resourceUrl = APP_CONFIG.FLASK_API + 'api/new-job/';
+
+    constructor(private http: HttpClient) {}
+
+    create(benchmark: IBenchmark): Observable<EntityResponseType> {
+        return this.http.post<IBenchmark>(this.resourceUrl, benchmark, { observe: 'response' });
+    }
+
+}
