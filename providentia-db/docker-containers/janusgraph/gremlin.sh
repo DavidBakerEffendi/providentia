7e66bb835adc16e0a86c5ac71b93dada3debaff3
DavidBakerEffendi <davidbakereffendi@hotmail.com>
2019-07-15 17:31:21 +0200

    Refactored project to be a lot more pythonic

--- providentia-db/docker-containers/janusgraph/gremlin.sh
+++ providentia-db/docker-containers/janusgraph/gremlin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --link prv-janusgraph:janusgraph -e GREMLIN_REMOTE_HOSTS=janusgraph -it --net janusgraph-docker_prv-network janusgraph/janusgraph:latest ./bin/gremlin.sh
+docker run --rm --link prv-janusgraph:janusgraph -e GREMLIN_REMOTE_HOSTS=janusgraph -it --net janusgraph_prv-network janusgraph/janusgraph:latest ./bin/gremlin.sh
