376478dfc8a22b98db34aaa4f2c4a84421677d0f
David Baker Effendi <davidbakereffendi@hotmail.com>
2019-08-22 21:55:22 +0200

    Improved SQL schema by adding many-to-many relationship for businesses and categories. This would handle the relationships better and improves on normalization.

--- providentia-db/import-tool/src/main/resources/schema/postgres-schema.sql
+++ providentia-db/import-tool/src/main/resources/schema/postgres-schema.sql
@@ -12,8 +12,9 @@ CREATE EXTENSION postgis;
 -- If already connected to the database, run from here to create the schema
 
 DROP TABLE IF EXISTS review;
-DROP TABLE IF EXISTS bus_by_cat;
+DROP TABLE IF EXISTS bus_2_cat;
 DROP TABLE IF EXISTS business;
+DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS friends;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS city;
@@ -41,10 +42,10 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
+-- Many-to-many table for user to user relationships
 CREATE TABLE friends (
     user_id CHARACTER VARYING(22),
     friend_id CHARACTER VARYING(22) NOT NULL,
-    PRIMARY KEY (user_id, friend_id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (friend_id) REFERENCES users (id)
 );
@@ -63,11 +64,20 @@ CREATE TABLE business (
     FOREIGN KEY (city) REFERENCES city (id)
 );
 
-CREATE TABLE bus_by_cat (
+CREATE TABLE category
+(
+    id   SERIAL,
+    name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Many-to-many table for business to category relationships
+CREATE TABLE bus_2_cat
+(
     business_id CHARACTER VARYING(22) NOT NULL,
-    category CHARACTER VARYING(50) NOT NULL,
-    PRIMARY KEY (business_id, category),
-    FOREIGN KEY (business_id) REFERENCES business (id)
+    category_id INTEGER               NOT NULL,
+    FOREIGN KEY (business_id) REFERENCES business (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
 CREATE TABLE review (
@@ -84,10 +94,13 @@ CREATE TABLE review (
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (business_id) REFERENCES business (id)
 );
--- Index spatio-temporal data
+
+-- Index spatio-temporal data and category names
 CREATE INDEX review_date_ind ON review (date);
 CREATE INDEX business_location_ind ON business USING GIST (location);
 CREATE INDEX business_city_ind ON business (city);
+CREATE INDEX category_name_ind ON category (name);
+
 -- Cluster reviews by date and businesses by location
 CLUSTER review USING review_date_ind;
 CLUSTER business USING business_city_ind;
