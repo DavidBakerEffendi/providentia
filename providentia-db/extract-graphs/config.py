ccf61f3eeef7197efc7f0804bb960c72b01200ae
David Baker Effendi <davidbakereffendi@hotmail.com>
2019-12-20 17:52:18 +0200

    Reran some results

--- providentia-db/extract-graphs/config.py
+++ providentia-db/extract-graphs/config.py
@@ -1,10 +1,10 @@
-PERC = '13'
-SETUP = 1
+PERC = '1.1'
+SETUP = 2
 KERN = 3
 
 if SETUP == 1:
     CONN = 'postgres://postgres:docker@0.0.0.0:5432/providentia'
 elif SETUP == 2:
-    CONN = 'postgres://postgres:docker@hydra.cs.sun.ac.za:5432/providentia'
-elif SETUP == 3:
     CONN = 'postgres://postgres:docker@35.177.36.36:5432/providentia'
+elif SETUP == 3:
+    CONN = 'postgres://postgres:docker@hydra.cs.sun.ac.za:5432/providentia'
